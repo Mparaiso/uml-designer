f7f069540be0e7c182796b634d3c1a7eafbc012d
mparaiso <aikah@free.fr>
2013-05-30 06:01:48 +0200

    content-editable 30/05/2013

--- www/static/vendor/uml-designer/uml-designer.js
+++ www/static/vendor/uml-designer/uml-designer.js
@@ -15,64 +15,106 @@ var exemple_de_model = {
 };
 var UD = {};
 
-UD.umlClassTemplate = "<div  class='uml-class'>\
+UD.umlClassTemplate = "\
 <div class='uml-class-handle'>&nbsp;</div>\
     <div class='uml-class-inner'>\
         <div class='uml-header'>\
        <%=model.type%>\
-        </div>\
+       </div>\
+       <div class='uml-class-select'><input type='checkbox'/></div>\
+       <div class='clear'></div>\
         <ul class='uml-property-list'>\
         <%for(prop in model.properties){%>\
             <li class='uml-cell uml-private'>\
-            <%=prop%>\
+            <%=model.properties[prop]%>\
             </li>\
             <% } %>\
         </ul>\
         <ul class='uml-method-list'>\
         <% for(method in model.methods){%>\
             <li class='uml-cell uml-public'>\
-            <%=method%>\
+            <%=model.methods[method]%>\
             </li>\
             <% } %>\
         </ul>\
-    </div>\
-</div>";
-UD.compiledUmlClassTemplate = _.template(UD.umlClassTemplate);
+    </div>";
 
-jQuery.fn.umlClass = (function (window, $, undefined, tpl) {
+UD.compiledUmlClassTemplate = _.template(UD.umlClassTemplate);
+/**
+ * jQuery.umlClass plugin
+ */
+(function ($, tpl) {
     "use strict";
     var defaultParams = {
         type: 'Class',
-        position: {x: 0, y: 0},
+        //position: {x: 0, y: 0},
         properties: [],
         methods: []
     };
-    /**
-     * @param {{type: String, position:{x:Number,y:Number},properties: String[], methods: String[] }} params
-     * @returns {*}
-     */
+    var setUmlModel = function (model) {
+        $(this).data("_model", model);
+        this.trigger("model:update", model);
+    }
+    var getUmlModel = function () {
+        return $(this).data("_model");
+    }
+    var renderUmlModel = function (event, model) {
+        var $this = $(this);
+        $this.cleanUmlCell();
+        $this.html(tpl({model: model}));
+        $(".uml-cell", $this).umlCell();
+        $(".uml-header", $this).umlCell();
+        $this.draggabilly({
+            containment: true,
+            handle: ".uml-class-handle"
+        });
+        if (!(this in umlClasses)) {
+            umlClasses.push(this);
+        }
+    }
+    var umlClasses = [];
     var umlClass = function (params) {
-        params = $.extend({}, defaultParams, params);
+        params = $.extend(defaultParams, params);
         $.each(this, function () {
-            var $this = this;
-            $this.html = tpl({model: params});
+            var $this = $(this);
+            $this.addClass("uml-class");
+            $this.on("model:update", $this.renderUmlModel);
+            $this.setUmlModel(params);
+
         });
         return this;
-    }
-    return umlClass;
-})
-    (window, $, undefined, UD.compiledUmlClassTemplate);
+    };
+    $.extend($.fn, {umlClass: umlClass,
+        getUmlModel: getUmlModel,
+        setUmlModel: setUmlModel,
+        renderUmlModel: renderUmlModel
+    });
+    $.getUmlClasses = function () {
+        return umlClasses;
+    };
+})($, UD.compiledUmlClassTemplate);
 /**
+ * jQuery.umlCell plugin
  * transform les cells en elements éditables
  */
-jQuery.fn.umlCell = (function (window, $, undefined) {
+(function ($) {
     "use strict";
     /**
      * FR : crée une zone éditable
      * @param {{editCallback:Function,uneditCallback:Function}} params
      * @returns {*}
      */
-    var cell = function (params) {
+
+    var cleanUmlCell = function () {
+        $.each(this, function () {
+            var $this = $(this);
+            $this.off('click');
+            $this.off('blur');
+            $this.off('celledit');
+            $this.off("cellunedit");
+        });
+    };
+    var umlCell = function (params) {
         params = params || {};
         $.each(this, function () {
             var editmode = false,
@@ -81,23 +123,30 @@ jQuery.fn.umlCell = (function (window, $, undefined) {
                 $this = $(this);
             $this.on("click", function () {
                 if (editmode === false) {
-                    $this.emit
-                    val = $this.text();
-                    $input = $("<input type='text'>");
-                    $input.val(val.trim());
-                    $this.html($input);
+                    $this.attr('contenteditable',true);
+//                    val = $this.text();
+//                    $input = $("<input type='text'  >");
+//                    $input.val(val.trim());
+//                    $this.html($input);
                     editmode = true;
                     $input.focus();
-                    $input.on("blur", function (event) {
+                    $this.on("blur",function(event){
                         if (editmode === true) {
-                            $this.html($input.val());
-                            editmode = false;
-                            event.preventDefault();
-                            $input.off("blur");
-                            $input = null;
-                            $this.trigger("cellunedit", $this, val);
+                        $this.attr('contenteditable',false)
+                        editmode=false;
+                        $this.trigger("cellunedit", $this, val);
                         }
                     });
+//                    $input.on("blur", function (event) {
+//                        if (editmode === true) {
+//                            $this.html($input.val());
+//                            editmode = false;
+//                            event.preventDefault();
+//                            $input.off("blur");
+//                            $input = null;
+//                            $this.trigger("cellunedit", $this, val);
+//                        }
+//                    });
                     $this.trigger("celledit", $this, val);
                 }
             });
@@ -111,26 +160,54 @@ jQuery.fn.umlCell = (function (window, $, undefined) {
 
         return this;
     }
-
-    return cell;
-})(window, jQuery, undefined);
-$(function () {
-    $(".uml-cell").umlCell({
-        editCallback: function () {
-            console.log("edit")
-        },
-        uneditCallback: function () {
-            console.log("unedit")
+    $.extend($.fn, {umlCell: umlCell, cleanUmlCell: cleanUmlCell});
+})(jQuery);
+/**
+ * jQuery.draggabilly plugin
+ */
+(function ($) {
+    var defaultParams = {};
+    var getDraggabilly = function () {
+        if (typeof(this._draggabilly) != undefined && this._draggabilly instanceof Draggabilly) {
+            return this._draggabilly;
         }
-    });
-    var umlClass1 = document.querySelector("#uml-class1");
-    var draggie = new Draggabilly(umlClass1, {
-        containment: true,
-        handle: ".uml-class-handle"
-    });
+    };
+    var draggabilly = function (params) {
+        $.extend(defaultParams, params);
+        $.each(this, function () {
+            this._draggabilly = new Draggabilly(this, params);
+        });
+        return this;
+    }
+    $.extend($.fn, {draggabilly: draggabilly, getDraggabilly: getDraggabilly});
+})(jQuery);
 
-
-})
-;
+$(function () {
+    var totalClass = 0;
+    var defaultModel = {
+        get type() {
+            return 'Class' + (totalClass++);
+        },
+        properties: ['+ prop1:String', '# prop2:Array', '- prop3:Int'],
+        methods: ['# method1:String', '+ method2:Array', '- method3:Int']
+    }
+    var $addClassButton = $('#add-class');
+    var $canvas = $("#uml-canvas");
+    var classes = [];
+    $addClassButton.on('click', function () {
+        var $class = $('<div class="uml-class">');
+        $class.umlClass(defaultModel);
+        $canvas.append($class);
+        classes.push($class);
+        $class.css("z-index", 10);
+        $class.on('mousedown', function () {
+            $.each(classes, function () {
+                this.css("z-index", 0);
+            });
+            $(this).css('z-index', 10);
+        });
+    });
+    $(".uml-class").umlClass(defaultModel);
+});
 
 
